474ea9fd3fa558b7a575b60c1e56bf2c30764798
Catalin-Diaconu <catalin12noi97@yahoo.com>
2022-05-13 23:52:28 +0300

    asfd

--- temp.php
+++ temp.php
@@ -4,7 +4,7 @@ $pass = "sfds";
 
 //$command = escapeshellcmd('python C:\wamp64\www\Project_php\index\main_user.py' . " " . $email);
    //  da   $cmd = 'python C:\wamp64\www\Project_php\index\test.py'.' '.$email.' '.$pass.' &';
-   $cmd = 'python C:\wamp64\www\Project_php\index\main_user1.py' . " " . $email.' &';
+   $cmd = 'python main_user1.py' . " " . $email.' &';
    $command = escapeshellcmd($cmd);
    $output = shell_exec($command);
    echo $output;
